8584a701d654058e050463ba8b4ccd4b956839a7
David Hernandez <davidamh@vt.edu>
2014-04-20 05:45:48 -0400

    Added hype bar; first version of receiver front-end complete with fixture mocks.

--- receiver/main-controller.js
+++ receiver/main-controller.js
@@ -9,6 +9,18 @@ angular.module('receiver')
             user: 'Butt Enthusiast'
         };
 
+        $scope.rating = 90;
+
+        $scope.goodRatingStyle = {
+            height: $scope.rating + '%',
+            width: '100%'
+        };
+
+        $scope.badRatingStyle = {
+            height: (100 - $scope.rating) + '%',
+            width: '100%'
+        };
+
         $scope.next = {
             song: {
                 title: 'Dat A S S',
